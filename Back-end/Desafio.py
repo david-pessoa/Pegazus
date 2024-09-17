b02e54c268d7ec374ee03a285628ed6cf9d235e0
david-pessoa <pessoa.dvlbp@gmail.com>
2024-09-17 12:31:55 -0300

    finaliza trabalho

--- Back-end/Desafio.py
+++ Back-end/Desafio.py
@@ -1,5 +1,3 @@
-#DESAFIO!!
-#crie uma interface de banco, o programa deve utilizar POO, a classe deve ter os atributos id, nome, cpf e saldo , aonde o saldo deve ser iniciado em 0, e o id deve ser autoicremental. a interfaçe deve permitir a criação de uma conta, e a realização das operações de saque e deposito do saldo da conta
 from cliente import Cliente
 from tkinter import *
 
